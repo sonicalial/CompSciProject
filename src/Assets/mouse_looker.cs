affa31e0e728db468fc0838dfcc9c5a8a8011bad
sonicalial <131232226+sonicalial@users.noreply.github.com>
2023-04-19 21:10:42 +0100

    Update mouse_looker.cs

--- src/Assets/mouse_looker.cs
+++ src/Assets/mouse_looker.cs
@@ -9,19 +9,20 @@ public class mouse_looker : MonoBehaviour
     void FixedUpdate()
     {
         timer = timer-1;
-        Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);//gets mouse position
         Vector3 dir = Input.mousePosition - pos;
-        Vector3 playerPos = GameObject.FindGameObjectsWithTag("Player")[0].transform.position;
-        transform.position = playerPos;
-        transform.position = Vector3.MoveTowards(GameObject.Find("Player").transform.position,dir,1);
-        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        if (Input.GetMouseButton(0))
+        Vector3 playerPos = GameObject.FindGameObjectsWithTag("Player")[0].transform.position;//gets the player location
+        transform.position = playerPos;//teleports the rectangle (gun) onto the player
+        transform.position = Vector3.MoveTowards(GameObject.Find("Player").transform.position,dir,1); //displaces the gun so that it points out one end not both
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;//tbh i dont know. it is important. stiched this together from many other sorces
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);//rotates the gun so it points toward the mouse using strange maths on line 17
+        
+        if (Input.GetMouseButton(0)) 
         {
             if (timer <= 0)
             {
-                Instantiate(bullet, transform.position, Quaternion.identity);
-                timer = 5;
+                Instantiate(bullet, transform.position, Quaternion.identity); //this "if" spawns a bullet when mouse is clicked and the cool down timer is down 
+                timer = 5;// cool down timer to prevent way to many bullets 
             }
         }
     }
